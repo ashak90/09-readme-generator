8bad47c7847f29d9b801130324856d0d5edd89b5
Asha Kumar <ashakumar90@gmail.com>
2021-04-16 13:38:17 -0400

    added the consts

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadme = require("./utils/generateMarkdown");
+const path = require('path');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
