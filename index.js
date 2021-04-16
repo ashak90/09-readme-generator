b6d525224145b1c53746a45d4d5b79eaa29adf2f
Asha Kumar <ashakumar90@gmail.com>
2021-04-16 13:42:49 -0400

    started the function to write the readme

--- index.js
+++ index.js
@@ -33,7 +33,14 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            console.log("Your responses:", data);
+
+        })
+}
 
 // Function call to initialize app
 init();
